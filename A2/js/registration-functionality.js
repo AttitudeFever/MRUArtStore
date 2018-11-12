536e086f94287dfa956c160b08960b21b5ac40cf
Hamid Hemani <hhema855@mtroyal.ca>
2018-11-12 21:30:37 +0000

    added art database into phpmyadmin

---
+++ A2/js/registration-functionality.js
@@ -0,0 +1,7 @@
+window.addEventListener('load' , ()=>{
+
+    document.getElementById('cancel').addEventListener('click', ()=>{
+
+        location.href = "index.php";
+    });
+});
--- A2/registration.php
+++ A2/registration.php
@@ -1,26 +1,29 @@
-
-
 <!DOCTYPE html>
 <html lang = "eng">
     <head>
-        <meta charset = "UTF-8"/>
-        
+        <meta charset ="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Registration</title>
+        <link rel = "stylesheet" href = "css/registration.css">
+        <script src ="js/registration-functionality.js"></script>
     </head>
-    <body>
-        
-        <form action="">
-        <fieldset>
-            <legend>Registration Page</legend>
-            First Name: <input type= "text" name="firstName" id="firstName"> <br>
-            Last Name: <input type= "text" name="lastName" id="lastName"> <br>
-            City: <input type= "text" name="city" id="city" id=city> <br>
-            Country: <input type="text" name="country" id="country"> <br>
-            E-mail: <input type="text" name="e-mail" id=email> <br>
-            Password: <input type="text" name="password" id=password><br>
-            <input type="submit" value="Sign up">
-        </fieldset>
-        
-        </form>
-     
+    <body id = registerBody>
+        <div id="register_panel">
+            <h2>Futureproof Registration for Premium Membership <img id="logo" src="images/logo.png"/></h2>
+                <div class = "RegisterFormBody" >
+                    <form method = "GET" action="" id="registrationForm">
+
+                            <label>First Name:</label> <input type= "text" name="firstName" id="firstName"> <br>
+                            <label>Last Name: </label><input type= "text" name="lastName" id="lastName"> <br>
+                            <label>City: </label><input type= "text" name="city" id="city" id=city> <br>
+                            <label>Country: </label><input type="text" name="country" id="country"> <br>
+                            <label> E-mail:</label> <input type="text" name="e-mail" id=email> <br>
+                            <label> Password: </label><input type="text" name="password" id=password><br>
+                            <input type="submit" value="Sign up">
+                            <button id="cancel" type="button">Cancel</button>
+
+                    </form>
+                </div>
+        </div>
     </body>
 </html>
